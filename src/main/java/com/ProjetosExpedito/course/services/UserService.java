258af07bb24d35d02244887dd7b91630d04800d5
expeditow <gabriel.expedito021@gmail.com>
2025-02-10 17:43:21 -0300

    Service layer, component registration

---
+++ src/main/java/com/ProjetosExpedito/course/services/UserService.java
@@ -0,0 +1,26 @@
+package com.ProjetosExpedito.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ProjetosExpedito.course.entities.User;
+import com.ProjetosExpedito.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
