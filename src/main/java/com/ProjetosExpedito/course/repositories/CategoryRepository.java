e4afd8e15bdad57e427fc6bdf354a43ca5c0cc50
expeditow <gabriel.expedito021@gmail.com>
2025-02-11 19:21:15 -0300

    Category

---
+++ src/main/java/com/ProjetosExpedito/course/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.ProjetosExpedito.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ProjetosExpedito.course.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
