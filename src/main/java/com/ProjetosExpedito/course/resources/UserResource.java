9fdeed30f91fb562f7d950bde5f34ddbc12bd18c
expeditow <gabriel.expedito021@gmail.com>
2025-02-13 16:50:01 -0300

    User delete

--- src/main/java/com/ProjetosExpedito/course/resources/UserResource.java
+++ src/main/java/com/ProjetosExpedito/course/resources/UserResource.java
@@ -5,13 +5,13 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.ProjetosExpedito.course.entities.User;
 import com.ProjetosExpedito.course.services.UserService;
@@ -40,5 +40,11 @@ public class UserResource {
 		obj = service.insert(obj);
 		//URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri(); -> Até resolver o problema do pacote
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id){
+		service.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 }
